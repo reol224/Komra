1c3ba751c5abe5e1b1316b0c60ca71d66b7069ab
reol224 <tmdoppelganger@gmail.com>
2025-09-28 02:12:09 +0000

    Fix config depreciation

--- next.config.js
+++ next.config.js
@@ -2,10 +2,15 @@
 
 const nextConfig = {
     images: {
-        domains: ['images.unsplash.com'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+                port: '',
+                pathname: '/**',
+            },
+        ],
     }
 };
-
-
 
 module.exports = nextConfig;
