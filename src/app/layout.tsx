ff7105c3559058e06b441aca7bd6aaf9489d3366
reol224 <tmdoppelganger@gmail.com>
2025-10-01 03:51:28 +0000

    Fix icons paths again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,9 +54,9 @@ export const metadata: Metadata = {
   },
   manifest: "/manifest.json",
   icons: {
-    icon: "images/icon-rounded-corners.png",
-    shortcut: "images/icon-rounded-corners.png",
-    apple: "images/icon-rounded-corners.png",
+    icon: "/images/icon-rounded-corners.png",
+    shortcut: "/images/icon-rounded-corners.png",
+    apple: "/images/icon-rounded-corners.png",
   },
   category: "technology",
 };
