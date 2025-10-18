203ac693b55a49f0c5b401d702ccd08e43689006
reol224 <tmdoppelganger@gmail.com>
2025-10-18 17:43:08 +0000

    Waitlist

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function HomePage() {
 
   useEffect(() => {
     // Redirect to waitlist page immediately
-    router.replace("/homepage");
+    router.replace("/waitlist");
   }, [router]);
 
   // Show loading state while redirecting
